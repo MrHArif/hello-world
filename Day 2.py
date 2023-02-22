7086d051f0c45033d0bdf22f9b0e58f13c84ce4a
MrHArif <haider.arif@aol.com>
2023-02-22 14:25:04 +0000

    Day 2, Lec 22: Mathematical Operators. TBC. Udemy

---
+++ Day 2.py
@@ -0,0 +1,51 @@
+#Day 2, Lec 19
+# Data types
+# String
+# subscripting
+# print("Hello"[0])
+# subscripting + 1
+# print("Hello"[0], "Hello"[1])
+# Integer
+# print(123+1)
+# float
+# print(3_12.3)
+# or anohter example of float
+# print(3.123)
+# Boolean
+# Lec 20
+# num_char = len(input("Full name: \n"))
+# print("Your name has " + str(num_char) + " characters")
+# data type checking
+# print(type(num_char))
+# typecasting
+# print(70.5+float("100.5"))
+# $realLife_func$
+# An app called YOU, holds all the details enjoyable for user and none for ill-users of data.
+# nam_char = len(input("Full name: \n"))
+# print("Your name has " + str(nam_char) + " characters")
+# Day 2, coding rooms exercise 1
+# 🚨 Don't change the code below 👇
+# two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+# digit_1 = int(two_digit_number[0])
+# digit_2 = int(two_digit_number[1])
+# total = digit_1 + digit_2
+# print(total)
+####################################
+# Lec 22, Mathematical operators
+print(3+5)
+print(7-4)
+print(6/3)
+print(2%5)
+print(2**3)
+# PEMDAS
+# (Calc to the left will be done first for * and / )
+# Parenthese ()
+# Exponents **
+# Multiplication * or */(same priority)
+# Division / or */(same priority)
+# Addition +
+# Subtraction -
+print(3*3+3/3-3)
+
+
